182981f2268d09ffa62e4f29a2b45091b2e55c02
MisoMaster11 <misohrbaty@gmail.com>
2025-11-03 21:36:02 +0100

    configurable background color
    added configurable variable background and implemented it onto every label and frame.

--- rel._odchylka_calculator.py
+++ rel._odchylka_calculator.py
@@ -2,10 +2,12 @@ import tkinter as tk
 from tkinter import messagebox
 
 presnost_na_desatine_miesta = 5
+background_color = 'gray95'
 
 root = tk.Tk()
 root.geometry("700x500")
 root.title("Relatívna odchylka")
+root.config(bg=background_color)
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
 root.rowconfigure(0, weight=1)
@@ -13,16 +15,16 @@ root.rowconfigure(1, weight=1)
 root.rowconfigure(2, weight=1)
 root.rowconfigure(3, weight=100)
 
-title = tk.Label(root, text='Relatívna odchylka', font=('Arial', 20), anchor='center')
+title = tk.Label(root, text='Relatívna odchylka', font=('Arial', 20), anchor='center', bg=background_color)
 title.grid(row=0, column=0, sticky='nsew', columnspan=2)
 
-entry_frame = tk.Frame(root)
+entry_frame = tk.Frame(root, bg=background_color)
 entry_frame.grid(row=1, column=0, sticky='nsew', columnspan=2)
 entry_frame.columnconfigure(0, weight=1)
 entry_frame.columnconfigure(1, weight=1)
 entry_frame.columnconfigure(2, weight=1)
 
-data_frame = tk.Frame(root)
+data_frame = tk.Frame(root, bg=background_color)
 data_frame.grid(row=2, column=0, sticky='nsew', columnspan=2)
 data_frame.columnconfigure(0, weight=1)
 data_frame.columnconfigure(1, weight=1)
@@ -46,11 +48,11 @@ deviationvar = tk.StringVar()
 deviation_listbox = tk.Listbox(root, font=('Arial', 15), listvariable=deviationvar)
 deviation_listbox.grid(row=3, column=1, sticky='nsew', padx=10, pady=10)
 
-averagel = tk.Label(data_frame, text='Priemerná hodnota\n', font=('Arial', 15))
+averagel = tk.Label(data_frame, text='Priemerná hodnota\n', font=('Arial', 15), bg=background_color)
 averagel.grid(row=0, column=0, sticky='nsew')
-avg_deviationl = tk.Label(data_frame, text='Priemerná odchylka\n', font=('Arial', 15))
+avg_deviationl = tk.Label(data_frame, text='Priemerná odchylka\n', font=('Arial', 15), bg=background_color)
 avg_deviationl.grid(row=0, column=1, sticky='nsew')
-relative_deviationl = tk.Label(data_frame, text='Relatívna odchylka\n', font=('Arial', 15))
+relative_deviationl = tk.Label(data_frame, text='Relatívna odchylka\n', font=('Arial', 15), bg=background_color)
 relative_deviationl.grid(row=0, column=2, sticky='nsew')
 
 data_list = []
