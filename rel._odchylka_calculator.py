84585961e1ec8ab4393f316f93bb5cad837e3597
MisoMaster11 <misohrbaty@gmail.com>
2025-11-12 09:06:44 +0100

    moved from tk to ctk

--- rel._odchylka_calculator.py
+++ rel._odchylka_calculator.py
@@ -1,16 +1,20 @@
 import tkinter as tk
+import customtkinter as ctk
 from tkinter import messagebox
 
+#=========== Configuration ===========#
 presnost_na_desatine_miesta = 5
 background_color = 'gray95'
 button_color = 'gray90'
 entry_and_list_color = 'white'
 text_color = 'black'
+#=====================================#
 
-root = tk.Tk()
+button_hover = '#%02x%02x%02x' % tuple(int(int(c,16)*0.8) for c in [button_color[1:3], button_color[3:5], button_color[5:7]])
+
+root = ctk.CTk(fg_color=background_color)
 root.geometry("700x500")
 root.title("Relatívna odchylka")
-root.config(bg=background_color)
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=1)
 root.rowconfigure(0, weight=1)
@@ -18,44 +22,44 @@ root.rowconfigure(1, weight=1)
 root.rowconfigure(2, weight=1)
 root.rowconfigure(3, weight=100)
 
-title = tk.Label(root, text='Relatívna odchylka', fg=text_color, font=('Arial', 20), anchor='center', bg=background_color)
+title = ctk.CTkLabel(root, text='Relatívna odchylka', text_color=text_color, font=('Arial', 30), anchor='center', fg_color=background_color)
 title.grid(row=0, column=0, sticky='nsew', columnspan=2)
 
-entry_frame = tk.Frame(root, bg=background_color)
+entry_frame = ctk.CTkFrame(root, fg_color=background_color)
 entry_frame.grid(row=1, column=0, sticky='nsew', columnspan=2)
 entry_frame.columnconfigure(0, weight=1)
 entry_frame.columnconfigure(1, weight=1)
 entry_frame.columnconfigure(2, weight=1)
 
-data_frame = tk.Frame(root, bg=background_color)
+data_frame = ctk.CTkFrame(root, fg_color=background_color)
 data_frame.grid(row=2, column=0, sticky='nsew', columnspan=2)
 data_frame.columnconfigure(0, weight=1)
 data_frame.columnconfigure(1, weight=1)
 data_frame.columnconfigure(2, weight=1)
 
-entry = tk.Entry(entry_frame, fg=text_color, font=('Arial', 15), bg=entry_and_list_color)
+entry = ctk.CTkEntry(entry_frame, text_color=text_color, font=('Arial', 20), fg_color=entry_and_list_color)
 entry.grid(row=1, column=0, sticky='nsew', pady=50, padx=10)
 entry.focus()
 
-enter = tk.Button(entry_frame, text='Enter', fg=text_color, font=('Arial', 15), bg=button_color)
+enter = ctk.CTkButton(entry_frame, text='Enter', hover_color=button_hover, text_color=text_color, font=('Arial', 20), fg_color=button_color)
 enter.grid(row=1, column=1, sticky='nsew', pady=50)
 
-clear = tk.Button(entry_frame, text='Clear', fg=text_color, font=('Arial', 15), bg=button_color)
+clear = ctk.CTkButton(entry_frame, text='Clear', text_color=text_color, hover_color=button_hover, font=('Arial', 20), fg_color=button_color)
 clear.grid(row=1, column=2, sticky='nsew', padx=10, pady=50)
 
 datavar = tk.StringVar()
-data_listbox = tk.Listbox(root, fg=text_color, font=('Arial', 15), listvariable=datavar, bg=entry_and_list_color)
+data_listbox = tk.Listbox(root, fg=text_color, font=('Arial', 20), listvariable=datavar, bg=entry_and_list_color)
 data_listbox.grid(row=3, column=0, sticky='nsew',padx=10, pady=10)
 
 deviationvar = tk.StringVar()
-deviation_listbox = tk.Listbox(root, fg=text_color, font=('Arial', 15), listvariable=deviationvar, bg=entry_and_list_color)
+deviation_listbox = tk.Listbox(root, fg=text_color, font=('Arial', 20), listvariable=deviationvar, background=entry_and_list_color)
 deviation_listbox.grid(row=3, column=1, sticky='nsew', padx=10, pady=10)
 
-averagel = tk.Label(data_frame, text='Priemerná hodnota\n', fg=text_color, font=('Arial', 15), bg=background_color)
+averagel = ctk.CTkLabel(data_frame, text='Priemerná hodnota\n', text_color=text_color, font=('Arial', 20), fg_color=background_color)
 averagel.grid(row=0, column=0, sticky='nsew')
-avg_deviationl = tk.Label(data_frame, text='Priemerná odchylka\n', fg=text_color, font=('Arial', 15), bg=background_color)
+avg_deviationl = ctk.CTkLabel(data_frame, text='Priemerná odchylka\n', text_color=text_color, font=('Arial', 20), fg_color=background_color)
 avg_deviationl.grid(row=0, column=1, sticky='nsew')
-relative_deviationl = tk.Label(data_frame, text='Relatívna odchylka\n', fg=text_color, font=('Arial', 15), bg=background_color)
+relative_deviationl = ctk.CTkLabel(data_frame, text='Relatívna odchylka\n', text_color=text_color, font=('Arial', 20), fg_color=background_color)
 relative_deviationl.grid(row=0, column=2, sticky='nsew')
 
 data_list = []
@@ -69,9 +73,9 @@ def list_to_stringvar(list = [], avg: float = 0, avg_deviation: float = 0, devia
     for i in deviations:
         strdeviations += '+' if i >= 0 else '- '
         strdeviations += str(round(abs(i), presnost_na_desatine_miesta)) + '\n'
-    averagel.config(text='Priemerná hodnota\n' + str(round(avg, presnost_na_desatine_miesta)))
-    avg_deviationl.config(text='Priemerná odchylka\n' + str(round(avg_deviation, presnost_na_desatine_miesta)))
-    relative_deviationl.config(text='Relatívna odchylka\n' + str(round(relative_deviation, presnost_na_desatine_miesta)) + "%")
+    averagel.configure(text='Priemerná hodnota\n' + str(round(avg, presnost_na_desatine_miesta)))
+    avg_deviationl.configure(text='Priemerná odchylka\n' + str(round(avg_deviation, presnost_na_desatine_miesta)))
+    relative_deviationl.configure(text='Relatívna odchylka\n' + str(round(relative_deviation, presnost_na_desatine_miesta)) + "%")
     deviationvar.set(strdeviations)
     datavar.set(strdata)
 
@@ -106,10 +110,10 @@ def clear_list(event = None):
     data_list = []
     list_to_stringvar(data_list)
 
-enter.config(command=add_to_list)
-clear.config(command=clear_list)
+enter.configure(command=add_to_list)
+clear.configure(command=clear_list)
 root.bind_all('<BackSpace>', clear_list)
 root.bind_all('<Delete>', clear_list)
 root.bind_all('<Return>', add_to_list)
 list_to_stringvar()
-tk.mainloop()
+root.mainloop()
